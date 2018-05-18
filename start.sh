762ea8e0437c8f8e130e2a1fb22daf4098c6f733
zhanghai630816 <35323615+zhanghai630816@users.noreply.github.com>
2018-05-18 12:56:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 geth --mine --minerthreads 2 --etherbase "0x0000000000000000000000000000000000000000" --rpc --rpcapi 'web3, eth ,personal, admin' nohup.out 2>&1  &
-echo '##在生成矿工账户并替换双引号中的账户地址后再启动本命令'
+echo '##需在geth console控制台生成一个账户后再启动本命令'
 
  
 
