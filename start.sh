4c7e95d2bb62b3eab50bc32dedf40e7d1c08e231
zhanghai630816 <35323615+zhanghai630816@users.noreply.github.com>
2018-05-21 23:50:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-nohup geth --mine --minerthreads 2 --etherbase "0x0000000000000000000000000000000000000000" --rpc --rpcapi 'web3,eth,net,personal,admin' nohup.out 2>&1  &
-echo '需在geth console控制台生成一个账户并exit退出后再启动本命令，使用方法见readme.md'
-
+geth --mine --minerthreads 2 --etherbase "0x0000000000000000000000000000000000000000" --rpc --rpcapi 'web3, eth ,personal, admin' nohup.out 2>&1  &
+echo '##需在geth console控制台生成一个账户并exit退出后再启动本命令，使用方法见readme.md'
  
 
 
